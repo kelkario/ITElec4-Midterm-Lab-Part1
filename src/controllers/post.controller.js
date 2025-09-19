88a1821d362e4f60125f8d5b6e1c61c9e398f239
Quishia Samantha Magpantay <ishi.magpantay@gmail.com>
2025-09-19 14:10:43 +0800

    validaton of input

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -22,10 +22,13 @@ export const getPostById = (req, res) => {
     res.json(post);
 };
 
-export const createPost = (req, res) => {
-    const { title, content } = req.body;
-    if (!title || !content) {
-        return res.status(400).json({ message: 'Title and content are required.' });
+export const createPost = async (req, res) => {
+    try {
+        // The data is guaranteed to be valid here
+        const newPost = await postService.createPost(req.body);
+        return res
+            .status(201)
+            .json(new ApiResponse(201, newPost, "Post created successfully"));
     }
     const newPost = postService.createPost({ title, content });
     res.status(201).json(newPost);
